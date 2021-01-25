ee8de5ff4e0f623ca2cd518d1a784407e132a325
Alison Pinheiro <alison.pinheiro@somagrupo.com.br>
2021-01-25 18:16:22 -0300

    add script and config

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-var gulp = require('gulp');
+const gulp        = require('gulp');
+const fileinclude = require('gulp-file-include');
+
+const paths = {
+  scripts: {
+    src: './emails',
+    dest: './build/'
+  }
+};
+
+async function includeHTML(){
+  return gulp.src([
+    '*.html',
+    '!header.html', // ignore
+    '!footer.html' // ignore
+    ])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+exports.default = includeHTML;
